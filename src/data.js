4fe023a976c865f8be0d17efd0b717aab06d8d8a
naveedur <naveedur1039.com>
2023-06-15 22:51:30 +0500

    first commit for deploying site

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://unsplash.com/photos/PNbDkQ2DDgM",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://unsplash.com/photos/PNbDkQ2DDgM",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "https://unsplash.com/photos/PNbDkQ2DDgM",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "https://unsplash.com/photos/PNbDkQ2DDgM",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "https://unsplash.com/photos/PNbDkQ2DDgM",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    link: "https://youtube.com/",
+  },
+];
