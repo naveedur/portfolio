d36656a2df1098a923148ba090ae760de2692bf4
naveedur <93273914+naveedur@users.noreply.github.com>
2023-07-07 11:13:27 +0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,31 +1,12 @@
 export const products = [
   {
     id: 1,
-    img: "https://unsplash.com/photos/PNbDkQ2DDgM",
-    link: "http://lama.dev",
+    img: "https://lh3.googleusercontent.com/pw/AJFCJaXOYJLA_Ho4fNvwuUfVnciDsr29OmlqwLMv4Auq36jShG3XlSYSBt7npUuaDCWtUjrTdkGO6r-lYqsFOcc-ysX89Zz52iuN24iXzDkDDBBlf7awhSoclSyNToXtt2kVH8lO8catlHRoI0xLT0hnWGt28AGcltjFxf0caA29xJoRjhJR6eWIZSQisIovhihZUjT5C9dNml6kuucpIQBRY8rJVrjem1a_xpiZdk8sz6jfyRvKOYOAmdOVBT1A8sQ0VTcAXgP1PkFXWpN8d-YMQyYAaqKPBX4efWun1fz4BAp12UUNhUaH7d1PPXwmqdnaq7uOH9uAySZX_k7cdGPMhlUhaIdqHHX3Z-sR9QWWQxN03KQdIuyYF06M8KAGtqW_o_lg7FzrI6AeUp1RbZ90LGsYCGEV4xpTh4yZDAYq0mDeNSpST1ISD4vHIUSylDN0C6iDrC07c0uZhNDcFJ3H3P-SnfJvU9sr8ehz95qu-8tLcYVo6DIK10yYWKuQADWARgi7ZBr_APb3D8i69-NVJ9Xy0q2HgBpFDVEoGZljc8YGR_Zh_OTjuFR2Igu2VR0V99fpkRCEkuxjtGEnXaBvqIFd3-A_JU8H50uUnizfGTZ2eCanAa7XbS_-uHRbnivSpUHph5w7D7S-UDRPx9YC5qUuGEM0J6yFDbwu7YzLHiow3MSJQuBaXfpz1oNlaJA-tU3xGnK9tfpgx6xVUWUTGp1W1XfaJUkqE4NWywEJsHVzGd9i0zwSKb3kXH2NZHwE7tExLra2p5tUC-lLsRRvDxATr6UXLlb7E8uqEIsCiXmEheSz-hkR-ceri6FZNKcOdwhfTsdtXqMJejBsEMa25Nuv3K89i5BddtunpNQUH7p-0l7SH-qLAAgJCYhmYXTComd5MQTTkxjGnl3MD4DDivVF7Q=w297-h625-s-no?authuser=0",
+    link: "https://wayfarer-world.vercel.app",
   },
+
   {
     id: 2,
-    img: "https://unsplash.com/photos/PNbDkQ2DDgM",
-    link: "http://lama.dev",
-  },
-  {
-    id: 3,
-    img: "https://unsplash.com/photos/PNbDkQ2DDgM",
-    link: "http://lama.dev",
-  },
-  {
-    id: 4,
-    img: "https://unsplash.com/photos/PNbDkQ2DDgM",
-    link: "http://lama.dev",
-  },
-  {
-    id: 5,
-    img: "https://unsplash.com/photos/PNbDkQ2DDgM",
-    link: "http://lama.dev",
-  },
-  {
-    id: 6,
     img: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
     link: "https://youtube.com/",
   },
