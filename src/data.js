bc020a0d9d67ece8448e131c7fd8a9780f5a3d99
naveedur <93273914+naveedur@users.noreply.github.com>
2023-07-09 10:00:42 +0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ export const products = [
 
   {
     id: 2,
-    img: "https://images.unsplash.com/photo-1628258334105-2a0b3d6efee1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-    link: "https://youtube.com/",
+    img: "https://photos.google.com/photo/AF1QipPPqE3Jc2TrQFoYfwuSEh5zmex2S0K4L-iSMijI",
+    link: "https://task-mastery.vercel.app/",
   },
 ];
